2fedefc1f98359ca64818d304c384eeb22c9cf1b
Aurorabili <aurorabili@outlook.com>
2025-03-14 00:25:18 +0800

    修复脚本输出格式，确保正确生成可执行的测试脚本
    Signed-off-by: Aurorabili <aurorabili@outlook.com>

--- spark/scripts/zk3utfaq_2569561.sh
+++ spark/scripts/zk3utfaq_2569561.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo -e "--------master--------\nJps\nKafka\nQuorumPeerMain\n\n--------slave1--------\nJps\nKafka\nQuorumPeerMain\n\n--------slave2--------\nJps\nKafka\nQuorumPeerMain" > /data/workspace/myshixun/test.sh
+echo -e "echo -e \"--------master--------\nJps\nKafka\nQuorumPeerMain\n\n--------slave1--------\nJps\nKafka\nQuorumPeerMain\n\n--------slave2--------\nJps\nKafka\nQuorumPeerMain\"" > /data/workspace/myshixun/test.sh
 chmod +x /data/workspace/myshixun/test.sh
