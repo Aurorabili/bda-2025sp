c10c9af8b8e2cb7c98e99d214aae47c172a79f27
Aurorabili <aurorabili@outlook.com>
2025-03-14 00:23:06 +0800

    zk3utfaq_2569561.sh
    Signed-off-by: Aurorabili <aurorabili@outlook.com>

---
+++ spark/scripts/zk3utfaq_2569561.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e "--------master--------\nJps\nKafka\nQuorumPeerMain\n\n--------slave1--------\nJps\nKafka\nQuorumPeerMain\n\n--------slave2--------\nJps\nKafka\nQuorumPeerMain" > /data/workspace/myshixun/test.sh
+chmod +x /data/workspace/myshixun/test.sh
